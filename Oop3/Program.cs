5b877cb53f5908ba425ddeaae7ae6ea1417eac86
mert delibalta <mertdelibalta@gmail.com>
2021-09-28 00:52:00 +0300

    Oop3 28.09.2021

---
+++ Oop3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+          
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerServices();
+            ILoggerService fileLoggerService = new FileLoggerServices();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager,  smsLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, konutKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+             
+
+
+        }
+    }
+}
